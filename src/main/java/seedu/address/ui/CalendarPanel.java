7f954937ae01869f4e96d07d01ee0f714f497e20
linnnruoo <linruo2014@gmail.com>
2018-10-22 01:19:07 +0800

    trim fxml

--- src/main/java/seedu/address/ui/CalendarPanel.java
+++ src/main/java/seedu/address/ui/CalendarPanel.java
@@ -1,9 +1,12 @@
 package seedu.address.ui;
+
 import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.view.CalendarView;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
+
 /**
  * @@ author: linnnruoo
  * Calendar view of the App
